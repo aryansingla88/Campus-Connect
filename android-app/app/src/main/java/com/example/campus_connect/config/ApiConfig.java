2bc1a64c7828ce1154f4a21e105b28837106eb20
Yatharth Chaudhary <yatharthchaudhary143@gmail.com>
2026-03-23 17:50:53 +0530

    Delete android-app directory

--- android-app/app/src/main/java/com/example/campus_connect/config/ApiConfig.java
+++
@@ -1,8 +0,0 @@
-package com.example.campus_connect.config;
-
-public class ApiConfig {
-
-    // Change this when backend server changes
-    public static final String BASE_URL = "http://10.142.44.112:8080";
-
-}
