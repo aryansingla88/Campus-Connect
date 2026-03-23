981e9ddfbbb4846856654d41a453868942d907d8
Yatharth Chaudhary <yatharthchaudhary143@gmail.com>
2026-03-23 16:04:17 +0530

    POI coordinates update

--- database/seed_pois.sql
+++ database/seed_pois.sql
@@ -1,44 +1,44 @@
 USE campus_connect;
 
 INSERT INTO poi (name, latitude, longitude, category) VALUES
-('Hostel-1', 31.2530, 75.7075, 'hostel'),
-('Hostel-2', 31.2531, 75.7076, 'hostel'),
-('Hostel-3', 31.2532, 75.7077, 'hostel'),
-('Hostel-4', 31.2533, 75.7078, 'hostel'),
-('Hostel-5', 31.2534, 75.7079, 'hostel'),
-('Hostel-6', 31.2535, 75.7080, 'hostel'),
+('Hostel-1', 29.94455889386729, 76.8203995311263, 'hostel'),
+('Hostel-2', 29.944746252423027, 76.81892101446383, 'hostel'),
+('Hostel-3', 29.946172377222155, 76.81944520061576, 'hostel'),
+('Hostel-4', 29.94764958325789, 76.81966962256365, 'hostel'),
+('Hostel-5', 29.947243609657765, 76.82127994841505, 'hostel'),
+('Hostel-6', 29.94610106716624, 76.82091349073112, 'hostel'),
 
-('Girls Hostel-1', 31.2540, 75.7085, 'hostel'),
-('Girls Hostel-2', 31.2541, 75.7086, 'hostel'),
+('Girls Hostel-1', 29.945286495531164, 76.81487588460747, 'hostel'),
+('Girls Hostel-2', 29.946305849234374, 76.81495619577504, 'hostel'),
 
-('Main Gate', 31.2500, 75.7040, 'landmark'),
-('Parking', 31.2503, 75.7038, 'facility'),
-('Professors Colony', 31.2520, 75.7068, 'residence'),
-('Market', 31.2515, 75.7059, 'facility'),
+('Main Gate', 29.9477003096136, 76.82241273682573, 'landmark'),
+('Parking', 29.948039479938604, 76.82201675217466, 'facility'),
+('Professors Colony', 29.94887901170387, 76.81991099893557, 'residence'),
+('Market', 29.948753512560028, 76.81841969074975, 'facility'),
 
-('Swimming Pool', 31.2522, 75.7062, 'sports'),
-('Badminton Court', 31.2523, 75.7064, 'sports'),
-('Sports Complex', 31.2525, 75.7066, 'sports'),
+('Swimming Pool', 29.950379675070355, 76.81882948328212, 'sports'),
+('Badminton Court', 29.950895606544016, 76.81954295089844, 'sports'),
+('Sports Complex', 29.95016586484367, 76.81586832451528, 'sports'),
 
-('Administrative Block', 31.2508, 75.7048, 'administration'),
+('Administrative Block', 29.949015347084664, 76.81735103991488, 'administration'),
 
-('Flag Park', 31.2510, 75.7050, 'park'),
-('Fountain Park', 31.2512, 75.7052, 'park'),
+('Flag Park', 29.948247907348495, 76.81707468222429, 'park'),
+('Fountain Park', 29.947341515325725, 76.81644168090791, 'park'),
 
-('Senate Hall', 31.2518, 75.7056, 'academic'),
-('Examination Hall', 31.2519, 75.7058, 'academic'),
-('OAT', 31.2521, 75.7060, 'events'),
+('Senate Hall', 29.947834221754132, 76.81806709956341, 'academic'),
+('Examination Hall', 29.947257095335257, 76.81807394362355, 'academic'),
+('OAT', 29.94669001462331, 76.81800420618735, 'events'),
 
-('Students Activity Centre', 31.2524, 75.7063, 'facility'),
-('Lecture Hall', 31.2517, 75.7055, 'academic'),
+('Students Activity Centre', 29.94544428494757, 76.81802029943854, 'facility'),
+('Lecture Hall', 29.944951566677346, 76.81690986490483, 'academic'),
 
-('MCA Department', 31.2516, 75.7053, 'academic'),
-('BTech CSE Department', 31.2514, 75.7051, 'academic'),
-('BTech Mechanical Department', 31.2513, 75.7049, 'academic'),
-('BTech Civil Department', 31.2511, 75.7047, 'academic'),
-('BTech Electronics Department', 31.2509, 75.7045, 'academic'),
+('MCA Department', 29.945039884284906, 76.81600864266589, 'academic'),
+('BTech CSE Department', 29.947861358114945, 76.81515570019556, 'academic'),
+('BTech Mechanical Department', 29.94640182481232, 76.81707616186749, 'academic'),
+('BTech Civil Department', 29.946518030475993, 76.8161856684624, 'academic'),
+('BTech Electronics Department', 29.946875943068207, 76.81622321938956, 'academic'),
 
-('Library', 31.2501, 75.7051, 'academic'),
-('Computer Centre', 31.2504, 75.7054, 'academic'),
+('Library', 29.94740450441878, 76.81523058760389, 'academic'),
+('Computer Centre', 29.948408123056463, 76.81553587220263, 'academic'),
 
-('Jubilee Hall', 31.2526, 75.7067, 'events');
+('Jubilee Hall', 29.946539837506787, 76.815287498692, 'events');
