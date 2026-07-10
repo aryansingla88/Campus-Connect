70e1b0b48205b864365d26cbcc56f7c7ffcd8eb9
PrathamYadav100 <prathamyadav.12drosary@gmail.com>
2026-07-10 23:59:00 +0530

    [feature/posts], implemented all the apis from the backend side

---
+++ backend/src/main/java/com/campus/Campus_Connect/config/WebConfig.java
@@ -0,0 +1,24 @@
+package com.campus.Campus_Connect.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+        Path uploadPath = Paths.get("uploads");
+
+        String uploadLocation =
+                uploadPath.toFile().getAbsolutePath();
+
+        registry.addResourceHandler("/uploads/**")
+                .addResourceLocations("file:" + uploadLocation + "/");
+    }
+}
